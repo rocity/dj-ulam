2dde69b50f561e840736aecbf63d82f843c8190f
rocity <hello@kvnpl.xyz>
2016-08-25 13:44:48 +0800

    Create unit test for homepage

--- ulam/urls.py
+++ ulam/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'ulam.views.home', name='home'),
+    url(r'^$', 'creator.views.home_page', name='creator_home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
 ]
