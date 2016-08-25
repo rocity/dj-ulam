f1ba0c18369c8869337562ec3b1a17c2c39b01fc
rocity <hello@kvnpl.xyz>
2016-08-25 13:36:21 +0800

    Create app for creating recipes

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,34 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTestCase(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_recipe_and_retrieve_it_later(self):
+        # Clyde has heard about a cool new online app where you can get random dishes
+        # to try and cook. He goes to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # He notices the page title mention Ulam
+        self.assertIn('Ulam', self.browser.title)
+        self.fail('Finish the test!')
+
+        # He is invited to create a recipe straight away
+
+        # He types on the title, "Adobo"
+
+        # He types on 3 ingredients
+
+        # Clyde wonders if the site will remember his recipe. Then he sees
+        # that the site has generated a unique URL for him
+
+        # He visits the URL and his recipe is still there
+
+        # Satisfied, he goes back to sleep
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
