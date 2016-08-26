16ef0a0ecd1630aee591d54cbc88ddd299db7d43
rocity <hello@kvnpl.xyz>
2016-08-26 09:20:49 +0800

    Create tests for creation of recipes

--- functional_tests.py
+++ functional_tests.py
@@ -15,13 +15,45 @@ class NewVisitorTestCase(unittest.TestCase):
 
         # He notices the page title mention Ulam
         self.assertIn('Ulam', self.browser.title)
-        self.fail('Finish the test!')
 
         # He is invited to create a recipe straight away
+        titlebox = self.browser.find_element_by_id('id_recipe_title')
+        self.assertEqual(
+            titlebox.get_attribute('placeholder'),
+            'What is your dish called?'
+            )
 
-        # He types on the title, "Adobo"
+        # He types on the title, "Apple Pie"
+        titlebox.send_keys('Apple Pie')
 
-        # He types on 3 ingredients
+        # He notices a field for the ingredients
+        recipebox = self.browser.find_element_by_id('id_recipe_ingre')
+        self.assertEqual(
+            titlebox.get_attribute('placeholder'),
+            'An ingredient for your dish?'
+            )
+
+        # He types an ingredient
+        recipebox.send_keys('Lemon Juice')
+
+        # When he clicks the Submit button, the page updates, and now the page
+        # displays "Apple Pie" as a dish, and "Lemon Juice" as its recipe
+        submitbutton = self.browser.find_element_by_id('id_submit_recipe')
+        submitbutton.click()
+
+        table = self.browser.find_element_by_id('id_recipe_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == 'Apple Pie' for row in rows)
+            )
+
+        self.assertTrue(
+            any(row.text == 'Lemon Juice' for row in rows)
+            )
+
+        # There is still a text box inviting him to add another ingredient.
+        # He enters "Sliced Apples"
+        self.fail('Finish the test!')
 
         # Clyde wonders if the site will remember his recipe. Then he sees
         # that the site has generated a unique URL for him
