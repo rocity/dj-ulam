d71ee5e84a892d5f98a51e09d7d4ac9a6ccc4c00
rocity <hello@kvnpl.xyz>
2016-08-29 09:40:18 +0800

    Refactor checking of table elements

--- functional_tests.py
+++ functional_tests.py
@@ -9,6 +9,11 @@ class NewVisitorTestCase(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_table(self, row_text):
+        table = self.browser.find_element_by_id('id_recipe_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_recipe_and_retrieve_it_later(self):
         # Clyde has heard about a cool new online app where you can get random dishes
         # to try and cook. He goes to check out its homepage
@@ -46,8 +51,8 @@ class NewVisitorTestCase(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_recipe_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('Apple Pie', [row.text for row in rows])
-        self.assertIn('Lemon Juice', [row.text for row in rows])
+        self.check_for_row_in_table('Apple Pie')
+        self.check_for_row_in_table('Lemon Juice')
 
         # There is still a text box inviting him to add another ingredient.
         # He enters "Sliced Apples"
